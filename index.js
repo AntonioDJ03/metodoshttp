716c6e6e220b48de413b4ffa3cf630f577599874
Antonio Jiménez Díaz <isc20350280@gmail.com>
2023-09-15 12:48:47 -0600

    actualizado

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require("express")
 const app = express()
 
 app.get("/",(req,res) => {
-    res.send("Hello, World! get")
+    res.json({
+        msg: 'Hello GET'
+    })
 })
 
 app.post("/",(req,res) => {
