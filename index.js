23817326405219c52f5a2d12381ad18b0d70793e
Antonio Jiménez Díaz <isc20350280@gmail.com>
2023-09-15 12:09:43 -0600

    metodoshttp

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express")
+
+const app = express()
+
+app.get("/",(req,res) => {
+    res.send("Hello, World! get")
+})
+
+app.post("/",(req,res) => {
+    res.send("Hello, World! post")
+})
+
+app.put("/",(req,res) =>{
+    res.send("Hello, World! put")
+})
+
+app.patch("/",(req,res) =>{
+    res.send("Hello, World! patch")
+})
+
+app.delete("/",(req,res) =>{
+    res.send("Hello, World! delete")
+})
+
+
+app.listen(3000,() =>{
+    console.log("listening on port 3000")
+})
